990a95fbd43f04f909af3da8159e7072b1db5479
Abhi Aryan <abhiaryanonnet7781@gmail.com>
2026-07-29 12:15:37 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ import streamlit as st
 st.set_page_config(
     page_title="AGNI Climate Digital Twin",
     page_icon="",
-    layout="wide",
-    initial_sidebar_state="collapsed"
+    layout="wide"
 )
 
 # ---------------- Hide Sidebar ---------------- #
